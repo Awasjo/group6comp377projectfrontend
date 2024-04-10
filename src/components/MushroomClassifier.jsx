cac209572292b822f2d1b489bb9311e9505167fb
you1424 <you1.bbbb@gmail.com>
2024-04-09 22:09:39 -0400

    added styles

--- src/components/MushroomClassifier.jsx
+++ src/components/MushroomClassifier.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import mushroomFeatures from './mushroomFeatures';
+import React, { useState } from "react";
+import axios from "axios";
+import mushroomFeatures from "./mushroomFeatures";
+import "./MushroomClassifier.css";
+import mushroomImage from "../images/kinoko_porcino_poruchini.png";
 
 const MushroomClassifier = () => {
-  const [prediction, setPrediction] = useState('');
+  const [prediction, setPrediction] = useState("");
   const [formData, setFormData] = useState(() => {
     // Initialize formData with empty values for each feature
     const initialFormData = {};
     for (const feature in mushroomFeatures) {
-      initialFormData[feature] = '';
+      initialFormData[feature] = "";
     }
     return initialFormData;
   });
@@ -17,7 +19,7 @@ const MushroomClassifier = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -30,36 +32,64 @@ const MushroomClassifier = () => {
         transformedData[feature] = [formData[feature]];
       }
       console.log(transformedData);
-  
+
       // Send the transformed data to the backend
-      const response = await axios.post('http://127.0.0.1:5000/predict', transformedData);
+      const response = await axios.post(
+        "http://127.0.0.1:5000/predict",
+        transformedData
+      );
       setPrediction(response.data.prediction);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
-  
 
   return (
     <div>
+      <img src={mushroomImage} alt="eringi" name="mashroom" />
+
       <h1>Mushroom Poisonous or Edible Classifier</h1>
       <form onSubmit={handleSubmit}>
         {/* Dynamically generate select inputs for each feature */}
-        {Object.entries(mushroomFeatures).map(([feature, { label, options }]) => (
-          <div key={feature}>
-            <label htmlFor={feature}>{label}</label>
-            <select name={feature} id={feature} value={formData[feature]} onChange={handleChange}>
-              <option value="">Select {label}</option>
-              {/* Dynamically generate options for each feature */}
-              {options.map(option => (
-                <option key={option.value} value={option.value}>{option.label}</option>
-              ))}
-            </select>
-          </div>
-        ))}
+        {Object.entries(mushroomFeatures).map(
+          ([feature, { label, options }]) => (
+            <div key={feature}>
+              <label htmlFor={feature}>{label}: </label>
+              <select
+                name={feature}
+                id={feature}
+                value={formData[feature]}
+                onChange={handleChange}
+              >
+                <option required value="">
+                  Select {label}
+                </option>
+                {/* Dynamically generate options for each feature */}
+                {options.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )
+        )}
         <button type="submit">Predict</button>
       </form>
-      {prediction && <p>Prediction: {prediction}</p>}
+
+      {prediction && (
+        <p
+          className={
+            prediction === "Edible"
+              ? "green-background"
+              : prediction === "Poisonous"
+              ? "red-background"
+              : ""
+          }
+        >
+          Prediction: {prediction}
+        </p>
+      )}
     </div>
   );
 };
